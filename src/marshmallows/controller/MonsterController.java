6e86bebaaab0fafd81a0220e78f9c0d4b0189433
Rinzler838 <dvil4688@740s-pl-105.local>
2015-09-17 09:08:55 -0600

    Added a monster maker based on user input

--- src/marshmallows/controller/MonsterController.java
+++ src/marshmallows/controller/MonsterController.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class MonsterController
 {
 	private MarshmallowMonster rinzlerMonster;
+	private MarshmallowMonster userMonster;
 	private MarshmallowOutput myOutput;
 	private Scanner monsterScanner;
 	
@@ -26,33 +27,70 @@ public class MonsterController
 	
 	public void start()
 	{
+		rinzlerMonster.toString();
 		myOutput.displayMonsterInfo(rinzlerMonster.toString());
 		askQuestions();
 		myOutput.displayMonsterInfo("New Monster Info: " + rinzlerMonster.toString());
 	}
 
 	private void askQuestions()
+
 	{
 		System.out.println("Type in a better name for the monster");
 		String betterMonsterName = monsterScanner.next();
-		rinzlerMonster.setmonsterName(betterMonsterName);
+		
 		System.out.println("Type in a different number of eyes for the monster");
 		int betterMonsterEyes = monsterScanner.nextInt();
-		rinzlerMonster.setmonsterEyes(betterMonsterEyes);
+		
 		System.out.println("Type in a different number of noses for the monster");
 		int betterMonsterNoses = monsterScanner.nextInt();
-		rinzlerMonster.setmonsterNoses(betterMonsterNoses);
+		
 		System.out.println("Type in a better number of hairs for the monster");
 		double betterMonsterHair = monsterScanner.nextDouble();
-		rinzlerMonster.setmonsterHair(betterMonsterHair);
+		
 		System.out.println("Type a better number of legs for the monster");
 		double betterMonsterLegs = monsterScanner.nextDouble();
-		rinzlerMonster.setmonsterLegs(betterMonsterLegs);
+		
 		System.out.println("Type in a better boolean for the monster's belly button");
 		boolean betterMonsterBellyButton = monsterScanner.nextBoolean();
-		rinzlerMonster.setmonsterBellyButton(betterMonsterBellyButton);
 		
+		rinzlerMonster.setmonsterName(betterMonsterName);
+		rinzlerMonster.setmonsterEyes(betterMonsterEyes);
+		rinzlerMonster.setmonsterHair(betterMonsterHair);
+		rinzlerMonster.setmonsterLegs(betterMonsterLegs);
+		rinzlerMonster.setmonsterBellyButton(betterMonsterBellyButton);
 	}
 	
+	/**
+	 * This method will gather the information to create an instance of a MarshmallowMonster.
+	 */
+	private void makeUserMonster()
+	{
+		//Step one: Get variables.
+		String userName;
+		int userEyes;
+		int userNoseCount;
+		double userHair;
+		double userLegs;
+		boolean userBellyButton;
+		
+		//Step two: Define the variables by using Scanner to get the users input.
+		System.out.println("Type in your monster's name.");
+		userName = monsterScanner.nextLine();
+		System.out.println("Type in the number of eyes your monster has.");
+		userEyes = monsterScanner.nextInt();
+		System.out.println("Type in the number of noses your monster has.");
+		userNoseCount = monsterScanner.nextInt();
+		System.out.println("Type in the amount of hair your monste has.");
+		userHair = monsterScanner.nextDouble();
+		System.out.println("Type in the number of legs your monster has");
+		userLegs = monsterScanner.nextDouble();
+		System.out.println("Type in whether your monster has a belly button (True), or not (False).");
+		userBellyButton = monsterScanner.nextBoolean();
+		
+		//Step three: Make a monster - use the Constructor. Remember that the order of the parameters matters.
+		userMonster = new MarshmallowMonster(userName, userEyes, userNoseCount, userHair, userLegs, userBellyButton);
+		
+	}
 	
 }
